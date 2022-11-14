e9f827c48a282a143d932b9644f53c1fdb6b68ff
stephenandary <stephen@jamminmusic.com>
2022-11-14 16:14:28 +0000

    add Redis install into postcreate commands

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -6,6 +6,18 @@ echo "Adding WASM Targets"
 rustup target add wasm32-unknown-unknown
 rustup target add wasm32-wasi
 
+echo "Installing Hombrew"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/vscode/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+
+echo "Installing NATS CLI"
+brew tap nats-io/nats-tools
+brew install nats-io/nats-tools/nats
+brew unlink nats && brew link nats
+
 echo "Installing Cargo-Watch"
 cargo install cargo-watch
 
@@ -25,5 +37,10 @@ cargo install --locked trunk
 echo "Installing uuid and psql for WASMcloud petclinic example"
 sudo apt-get -y install uuid-runtime
 sudo apt-get -y install postgresql
+
+echo "Creating Docker-in-Docker Redis Server"
+docker network create -d bridge redisnet
+docker run -d -p 6379:6379 --name myredis --network redisnet redis
+brew install redis-cli
 
 echo "Done!"
